dddd39084c59a970ea832a39b7e0e122dbe8c7f2
rempek99 <arkadiusz.remplewicz@gmail.com>
2021-01-24 21:28:03 +0100

    Setup.py

--- Wolf/setup.py
+++ Wolf/setup.py
@@ -8,6 +8,5 @@ setup(
     author_email='224413@edu.p.lodz.pl',
     url="https://github.com/rempek99/python_tutorial",
     py_modules=['__main__', 'Parser'],
-    data_files=[('config', ['config'])],
     packages=find_packages(),
 )
